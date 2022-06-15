5d0da51217dab92ae1384455c6169afd279da5b2
Ilya Taratukhin <darkilfa@gmail.com>
2022-06-15 11:20:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<p align="center">
+  <a href="https://fingerprintjs.com">
+    <img src="res/logo.svg" alt="FingerprintJS" width="312px" />
+  </a>
+</p>
+<p align="center">
+   <a href="https://opensource.org/licenses/MIT">
+    <img src="https://img.shields.io/:license-mit-blue.svg?style=flat"/>
+  </a>
+  <a href="https://discord.gg/39EpE2neBg">
+    <img src="https://img.shields.io/discord/852099967190433792?style=logo&label=Discord&logo=Discord&logoColor=white" alt="Discord server">
+  </a>
+</p>
+
+# Fingerprint Pro Server API OpenAPI
+
+Definition for the Fingerprint Pro Server API in [OpenAPI](https://swagger.io/docs/specification/about/) format.
+
+## WIP: The repository is in the development
+
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const outputPath = path.resolve(__dirname, 'dist');
+
+export default {
+  mode: 'development',
+  entry: {
+    app: './src/index.ts',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.yaml$/,
+        use: [
+          // { loader: 'json-loader' },
+          { loader: 'yaml-loader' },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [outputPath],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'schemes', to: 'schemes' }],
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: outputPath,
+  },
+};
