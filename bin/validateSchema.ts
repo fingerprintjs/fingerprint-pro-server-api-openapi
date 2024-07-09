4f4b2bb6f60e91d23c8fdd42e718c64aeae13e39
leenasudhakar <137198769+leenasudhakar@users.noreply.github.com>
2024-07-09 04:43:23 -0400

    DOC-39: Address feedback, refactoring for 403. (#106)
    * Create get_related_visitors_200_empty.json
    
    Add an example for when there are no visitors that are related to the given visitor ID.
    
    * Update fingerprint-related-visitors-api.yaml
    
    Include an example for when the given visitor ID does not have any related visitors.
    
    * Validate get_related_visitors_200_empty.json
    
    * Create example file 403_error_wrong_region.json
    
    * Create example file 403_error_token_required.json
    
    * Create example file 403_error_token_not_found.json
    
    * Rename examples/delete_visits_403_error.json to examples/shared/403_error_feature_not_enabled.json
    
    * Include more examples for 403 in fingerprint-related-visitors-api.yaml
    
    * Provide more 403 examples in fingerprint-server-api.yaml
    
    * Update validateSchema.ts to validate against the new 403 examples
    
    * Don't know what is causing the self installation error
    
    * ci: fix lint
    
    * Update examples/related-visitors/get_related_visitors_200_empty.json
    
    Co-authored-by: Juraj Uhlar <juro.uhlar@gmail.com>
    
    * Update summary for 403-wrong-region
    
    * Update summary for 403-wrong-region
    
    * Mobile Smart Signals responses are now available to all plans.
    
    ---------
    
    Co-authored-by: Juraj Uhlar <juro.uhlar@gmail.com>

--- bin/validateSchema.ts
+++ bin/validateSchema.ts
@@ -209,7 +209,13 @@ async function validateCommonError403Schema(testSubscriptions: TestSubscription[
   const commonError403Validator = ajv.compile(commonError403Schema);
 
   // Validate against example file
-  ['./examples/get_event_403_error.json', './examples/delete_visits_403_error.json'].forEach((examplePath) =>
+  [
+    './examples/get_event_403_error.json',
+    './examples/shared/403_error_feature_not_enabled.json',
+    './examples/shared/403_error_token_not_found.json',
+    './examples/shared/403_error_token_required.json',
+    './examples/shared/403_error_wrong_region.json',
+  ].forEach((examplePath) =>
     validateJson({
       json: JSON.parse(fs.readFileSync(examplePath).toString()),
       jsonName: examplePath,
@@ -512,7 +518,10 @@ async function validateRelatedVisitorsResponseSchema(testSubscriptions: TestSubs
   const relatedVisitorsResponseValidator = ajv.compile(relatedVisitorsResponseSchema);
 
   // Validate against example file
-  ['./examples/related-visitors/get_related_visitors_200.json'].forEach((examplePath) =>
+  [
+    './examples/related-visitors/get_related_visitors_200_empty.json',
+    './examples/related-visitors/get_related_visitors_200.json',
+  ].forEach((examplePath) =>
     validateJson({
       json: JSON.parse(fs.readFileSync(examplePath).toString()),
       jsonName: examplePath,
