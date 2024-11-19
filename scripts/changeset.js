70f19885d599100fe34c1f6c3931da6ea246199b
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2024-11-19 09:52:41 +0100

    chore: don't create changeset for empty description

--- scripts/changeset.js
+++ scripts/changeset.js
@@ -58,8 +58,8 @@ const data = await prompt([
 ]);
 
 const description = data.scope ? `**${data.scope}**: ${data.description}` : data.description;
-
-const changeset = `
+if (description) {
+  const changeset = `
 ---
 '${pkg.name}': ${data.version}
 ---
@@ -68,9 +68,12 @@ ${description}
 
   `.trim();
 
-const fileName = `${humanId({ separator: '-', capitalize: false })}.md`;
-const filePath = `.changeset/${fileName}`;
+  const fileName = `${humanId({ separator: '-', capitalize: false })}.md`;
+  const filePath = `.changeset/${fileName}`;
 
-fs.writeFileSync(filePath, changeset, 'utf-8');
+  fs.writeFileSync(filePath, changeset, 'utf-8');
 
-console.info(`✅ Created changeset ${fileName}. \n\n${changeset}`);
+  console.info(`✅ Created changeset ${fileName}. \n\n${changeset}`);
+} else {
+  console.info(`❌ Changeset creation cancelled`);
+}
