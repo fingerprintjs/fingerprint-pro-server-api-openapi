872cd056cacb44b0bf301234ab85e744816e914a
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2025-10-08 15:06:00 +0200

    chore: update swagger-ui, use schema v4 in swagger-ui

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import 'swagger-ui/dist/swagger-ui.css';
 
 const ui = SwaggerUI({
   dom_id: '#swagger',
-  url: 'schemas/fingerprint-server-api.yaml',
+  url: 'schemas/fingerprint-server-api-v4-with-examples.yaml',
   onComplete: () => ui.preauthorizeApiKey('ApiKeyQuery', process.env.PRIVATE_KEY),
 });
