ee768766eaa98e6fa2f71de6a30e8eebf3d65e64
Eray Aydın <eray.aydin@fingerprint.com>
2026-02-19 16:57:22 +0300

    refactor: rename mock files for additional properties
    Rename test mock files to better describe their purpose in testing
    the `additionalProperties` removal functionality.
    
    Related-Task: INTER-1814

--- utils/transformers/removeFieldTransformer.spec.js
+++ utils/transformers/removeFieldTransformer.spec.js
@@ -3,8 +3,10 @@ import { transformSchema } from './transformSchema.js';
 import { removeFieldTransformer } from './removeFieldTransformer.js';
 
 const simpleYaml = fs.readFileSync('./utils/mocks/simple.yaml');
-const simpleWithRequest = fs.readFileSync('./utils/mocks/simpleWithRequest.yaml');
-const simpleWithoutAdditionalProperties = fs.readFileSync('./utils/mocks/simpleWithoutAdditionalProperties.yaml');
+const simpleWithAdditionalProperties = fs.readFileSync('./utils/mocks/simpleWithAdditionalProperties.yaml');
+const simpleWithAdditionalPropertiesCleaned = fs.readFileSync(
+  './utils/mocks/simpleWithAdditionalPropertiesCleaned.yaml'
+);
 const schemaWithXReadme = fs.readFileSync('./utils/mocks/schemaWithXReadme.yaml');
 const schemaWithXReadmeCleaned = fs.readFileSync('./utils/mocks/schemaWithXReadmeCleaned.yaml');
 
@@ -23,7 +25,7 @@ describe('Test removeFieldTransformer', () => {
   });
 
   it('removes field only when value matches expectedValue', () => {
-    const result = removeAdditionalPropertiesFalseFromSchema(simpleWithRequest);
-    expect(result.toString()).toEqual(simpleWithoutAdditionalProperties.toString());
+    const result = removeAdditionalPropertiesFalseFromSchema(simpleWithAdditionalProperties);
+    expect(result.toString()).toEqual(simpleWithAdditionalPropertiesCleaned.toString());
   });
 });
