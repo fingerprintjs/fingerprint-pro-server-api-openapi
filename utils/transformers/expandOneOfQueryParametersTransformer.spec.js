4d06c33639ef2f2ccbeaa94527018d9f8269d21c
Dan McNulty <daniel.mcnulty@fingerprint.com>
2026-04-14 14:37:49 -0500

    chore: add expandOneOfQueryParametersTransformer
    - Add new `expandOneOfQueryParametersTransformer` with unit test cases

---
+++ utils/transformers/expandOneOfQueryParametersTransformer.spec.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import { transformSchema } from './transformSchema.js';
+import { expandOneOfQueryParametersTransformer } from './expandOneOfQueryParametersTransformer.js';
+
+const simpleYaml = fs.readFileSync('./utils/mocks/simple.yaml');
+const schemaWithOneOfQueryParameter = fs.readFileSync('./utils/mocks/schemaWithOneOfQueryParameter.yaml');
+const schemaWithOneOfQueryParameterTransformed = fs.readFileSync(
+  './utils/mocks/schemaWithOneOfQueryParameterTransformed.yaml'
+);
+
+const applyTransformer = (yaml) => transformSchema(yaml, [expandOneOfQueryParametersTransformer]);
+
+describe('Test expandOneOfQueryParametersTransformer', () => {
+  it('does not modify schema without oneOf query parameters', () => {
+    const result = applyTransformer(simpleYaml);
+    expect(result.toString()).toEqual(simpleYaml.toString());
+  });
+
+  it('expands oneOf query parameters into separate parameters', () => {
+    const result = applyTransformer(schemaWithOneOfQueryParameter);
+    expect(result.toString()).toEqual(schemaWithOneOfQueryParameterTransformed.toString());
+  });
+});
